19ad81480f9cda84a3fd40249eebd17fba1c6b48
judiw <root@us1.judi.fun>
2019-12-27 08:41:03 +0800

    Update

--- upload.sh
+++ upload.sh
@@ -3,7 +3,7 @@ wget https://raw.githubusercontent.com/17mon/china_ip_list/master/china_ip_list.
 sed '/./{s/^/IP-CIDR,&/;s/$/&,no-resolve/}' /www/wwwroot/sub/china_ip_list.txt > /www/wwwroot/sub/china_ip.list
 cd /www/wwwroot/sub/
 /usr/bin/git add china_ip.list
-/usr/bin/git add cnip.sh
+/usr/bin/git add upload.sh
 /usr/bin/git add pref.ini
 /usr/bin/git commit -m "Update from IPIP.net"
 /usr/bin/git push -u origin master
