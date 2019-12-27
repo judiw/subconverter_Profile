94f0d332e45770f3d9fe0876ee5a3ba3c460a05b
judiw <root@us1.judi.fun>
2019-12-27 10:09:13 +0800

    Auto Update

--- upload.sh
+++ upload.sh
@@ -1,6 +1,6 @@
 #/bin/bash
 wget https://raw.githubusercontent.com/17mon/china_ip_list/master/china_ip_list.txt -O /www/wwwroot/sub/china_ip_list.txt
-sed '/./{s/^/IP-CIDR,&/;s/$/&,no-resolve/}' /www/wwwroot/sub/china_ip_list.txt > /www/wwwroot/sub/china_ip.list
+sed 's/^/IP-CIDR,&/g' /www/wwwroot/sub/china_ip_list.txt > /www/wwwroot/sub/china_ip.list
 cd /www/wwwroot/sub/
 /usr/bin/git add china_ip.list
 /usr/bin/git add upload.sh
