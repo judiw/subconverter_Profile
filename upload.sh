70ddc9a85657f9e016954d96ca9a3466f88f7676
judiw <root@us1.judi.fun>
2019-12-31 22:08:24 +0800

    Auto Update

--- upload.sh
+++ upload.sh
@@ -1,12 +1,12 @@
 #/bin/bash
-wget https://raw.githubusercontent.com/17mon/china_ip_list/master/china_ip_list.txt -O /www/wwwroot/sub/china_ip_list.txt
-sed 's/^/IP-CIDR,&/g' /www/wwwroot/sub/china_ip_list.txt > /www/wwwroot/sub/china_ip.list
+/bin/wget https://raw.githubusercontent.com/17mon/china_ip_list/master/china_ip_list.txt -O /www/wwwroot/sub/china_ip_list.txt
+/bin/sed 's/^/IP-CIDR,&/g' /www/wwwroot/sub/china_ip_list.txt > /www/wwwroot/sub/china_ip.list
 cd /www/wwwroot/sub/
-/usr/bin/git add china_ip.list
-/usr/bin/git add upload.sh
-/usr/bin/git add pref.ini
-/usr/bin/git add blocked_aliyun.list
-/usr/bin/git add Media_JP.list
-/usr/bin/git add game.list
-/usr/bin/git commit -m "Auto Update"
-/usr/bin/git push -u origin master
+/bin/git add china_ip.list
+/bin/git add upload.sh
+/bin/git add pref.ini
+/bin/git add blocked_aliyun.list
+/bin/git add Media_JP.list
+/bin/git add game.list
+/bin/git commit -m "Auto Update"
+/bin/git push -u origin master
