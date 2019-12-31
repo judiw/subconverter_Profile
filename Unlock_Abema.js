e6231e6c9e2faa3d303456186ab654eebbbebbdb
judiw <root@us1.judi.fun>
2019-12-31 22:05:24 +0800

    Auto Update

--- Unlock_Abema.js
+++ Unlock_Abema.js
@@ -1,15 +1,14 @@
 // ==UserScript==
-// [url=home.php?mod=space&uid=5839]@name[/url] Abema 区域限制
+// @name Abema 区域限制
 // @version 1.0.0
 // @run-at document-end
 // @namespace Violentmonkey Scripts
 // @match https://abema.tv/*
 // @grant none
 // ==/UserScript==
-
 Object.defineProperty(__CLIENT_REGION__, 'isAllowed', {
   get: () => true
 });
 Object.defineProperty(__CLIENT_REGION__, 'status', {
-  get: () => true
+  get: () => false
 });
