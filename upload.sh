981120ff9644236f9720738b24f00e5126c704fb
judiw <root@us1.judi.fun>
2020-02-24 11:29:37 +0800

    Version Update

--- upload.sh
+++ upload.sh
@@ -1,12 +1,14 @@
 #/bin/bash
-/bin/wget https://raw.githubusercontent.com/17mon/china_ip_list/master/china_ip_list.txt -O /www/wwwroot/sub/china_ip_list.txt
-/bin/sed 's/^/IP-CIDR,&/g' /www/wwwroot/sub/china_ip_list.txt > /www/wwwroot/sub/china_ip.list
+/bin/wget https://raw.githubusercontent.com/17mon/china_ip_list/master/china_ip_list.txt -O /www/wwwroot/sub/rules/china_ip_list.txt
+/bin/sed 's/^/IP-CIDR,&/g' /www/wwwroot/sub/rules/china_ip_list.txt > /www/wwwroot/sub/rules/china_ip.list
 cd /www/wwwroot/sub/
-/bin/git add china_ip.list
+/bin/git add rules/china_ip.list
 /bin/git add upload.sh
 /bin/git add pref.ini
-/bin/git add ip_block.list
-/bin/git add Media_JP.list
-/bin/git add game.list
+/bin/git add Unlock_Abema.js
+/bin/git add pref-new.yml
+/bin/git add rules/ip_block.list
+/bin/git add rules/Media_JP.list
+/bin/git add rules/game.list
 /bin/git commit -m "Auto Update"
 /bin/git push -u origin master
